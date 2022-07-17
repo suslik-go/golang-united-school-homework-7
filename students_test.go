38028364c42d185bcd6498b291cdf9e23e7b4f2a
suslik <hasmik_margaryan@epam.com>
2022-07-17 03:12:53 -0700

    Initial solution

--- students_test.go
+++ students_test.go
@@ -2,6 +2,9 @@ package coverage
 
 import (
 	"os"
+	//"errors"
+	"testing"
+	"time"
 )
 
 // DO NOT EDIT THIS FUNCTION
@@ -16,5 +19,81 @@ func init() {
 	}
 }
 
-// WRITE YOUR CODE BELOW
 
+
+//Len returns the length of input value
+func TestLen(t *testing.T) {
+	var persons People
+	
+	if persons.Len() != len(persons) {
+		t.Errorf("Failed Len function")
+	}
+}
+
+//Less reports when it is necessary to sort element i before element j
+// time year int, month Month, day, hour, min, sec, nsec int, loc *Location
+func TestLess(t *testing.T) {
+	var persons People
+	persons = append(persons, Person{ "Adam" , "Brown" , time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)})
+	persons = append(persons, Person{ "Adam" , "Atkinson" , time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)})
+	persons = append(persons, Person{ "Billy" , "Brown" , time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)})
+	persons = append(persons, Person{ "Billy" , "Brown" , time.Date(1999, time.January, 1, 0, 0, 0, 0, time.UTC)})
+
+	if persons.Less(1,0) == false {
+		t.Errorf("Failed Less function")
+	}
+
+	if persons.Less(0,2) == false {
+		t.Errorf("Failes Less function")
+	}
+
+	if persons.Less(3,0) {
+		t.Errorf("Failed Less function")
+	}
+
+}
+
+//Swap swaps the elements with indexes i and j.
+func TestSwap(t *testing.T) {
+	var persons People
+	persons = append(persons, Person{ "Adam" , "Brown" , time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)})
+	persons = append(persons, Person{ "Carol" , "Atkinson" , time.Date(2001, time.January, 1, 0, 0, 0, 0, time.UTC)})
+	
+	
+	persons.Swap(0,1)
+
+	// if persons[0].firstName != "Carol" || persons[0].lastName != "Atkinson" || persons[0].birthDay.Equal(time.Date(2001, time.January, 1, 0, 0, 0, 0, time.UTC)) {
+	// 	t.Errorf("Failed Swap function")
+	// }
+
+}
+
+/*New creates a matrix from a string.*/
+func TestNew(t *testing.T) {
+
+	// testCases := []struct{
+	// 	Input string
+	// 	Expected struct{
+	// 		Error error
+	// 		OutputMatrix *Matrix
+	// 	}
+	// }{
+		
+	// }
+
+}
+
+
+/*Rows gets the matrix represented in rows.*/
+func TestRows(t *testing.T) {
+}
+
+
+/*Cols gets the matrix represented in columns*/
+func TestCols(t *testing.T) {
+}
+
+
+/*Set sets the value of the matrix at point row, col.*/
+func TestSet(t *testing.T) {
+}
